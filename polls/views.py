8b852b135b2097ce67023d3a21076786b0e1a1ff
happy.zhang <ihzlpy@gmail.com>
2017-03-26 17:27:49 +0800

    tutorial 6

--- polls/views.py
+++ polls/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
-
+from django.utils import timezone
 from .models import Choice, Question
 
 class IndexView(generic.ListView):
