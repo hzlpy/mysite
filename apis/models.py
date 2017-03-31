0db119571c71d74f2b48aa6e61e34479d42326b6
happy.zhang <ihzlpy@gmail.com>
2017-04-01 00:12:37 +0800

    修改 model

--- apis/models.py
+++ apis/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Project(models.Model):
     project_name = models.CharField(max_length=200)
-    add_project_name = models.DateTimeField('date project added', null=True)
-    
+    add_project_date = models.DateTimeField('date project added', null=True)
+
     def __str__(self):
         return self.project_name
 
